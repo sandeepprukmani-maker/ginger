03dd178188e2f06a4ce5a92c76b358b65238ba9b
Sandeep <sandeep.p.rukmani@gmail.com>
2025-10-13 02:19:12 +0530

    first commit

--- run_server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Entry point for running the VisionVault server."""
-
-from visionvault.web.app import app, socketio
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 7890))
-    socketio.run(app, port=port, debug=False, allow_unsafe_werkzeug=True)
