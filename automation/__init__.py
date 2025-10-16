0747461bb8a8f5d0b177556b131fe63465b484ac
Sandeep <sandeep.p.rukmani@gmail.com>
2025-10-17 01:08:06 +0530

    gfd

--- automation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Browser Automation Engine using Playwright MCP and LLM
-"""
-
-from .automation_engine import AutomationEngine
-from .mcp_client import PlaywrightMCPClient
-from .llm_orchestrator import LLMOrchestrator
-
-__all__ = ['AutomationEngine', 'PlaywrightMCPClient', 'LLMOrchestrator']
