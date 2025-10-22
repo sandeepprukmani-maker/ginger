7b0095e6e201744464b3ba5f27a4edbbf3e45951
Sandeep <sandeep.p.rukmani@gmail.com>
2025-10-22 19:14:34 +0530

    5678

--- src/web/services/playwright_mcp/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Playwright MCP Engine Module
-Model Context Protocol-based browser automation using Playwright
-"""
-from .engine import MCPAutomationEngine
-from .client import MCPStdioClient
-from .browser_agent import BrowserAgent
-
-__all__ = ['MCPAutomationEngine', 'MCPStdioClient', 'BrowserAgent']
