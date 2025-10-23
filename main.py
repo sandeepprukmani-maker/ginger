615146cdd8286e8c96cbda9c536411c726af4c91
Sandeep <sandeep.p.rukmani@gmail.com>
2025-10-24 03:10:38 +0530

    234

--- main.py
+++
@@ -1,6 +0,0 @@
-from application import app, socketio, logger
-
-if __name__ == '__main__':
-    logger.info("Starting Self-Healing Browser Automation System")
-    logger.info(f"Dashboard available at: http://0.0.0.0:5000")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
