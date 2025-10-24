6d31f4f2fbb1d48947f334a3c598eefd7bf152e5
Sandeep <sandeep.p.rukmani@gmail.com>
2025-10-24 06:55:19 +0530

    ghj

--- main.py
+++
@@ -1,5 +0,0 @@
-from application import app, socketio, logger
-
-if __name__ == '__main__':
-    logger.info("Starting Self-Healing Browser Automation System")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True, use_reloader=True, log_output=True)
